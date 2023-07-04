66d322ce6a8c9ed66581c43c273b68025becdaa2
zhousb138912 <1292367332@qq.com>
2023-07-04 18:59:02 +0800

    修改readme

--- init.sh
+++ init.sh
@@ -3,9 +3,11 @@ mkdir -p /server/data/mysql8
 mkdir -p /server/data/redis
 mkdir -p /server/etc/php8.1.19/php.d
 mkdir -p /server/log/mongodb6
-touch /server/log/mongodb6/mongodb.log
 mkdir -p /server/log/mysql8
 mkdir -p /server/log/nginx
 mkdir -p /server/log/php8.1
 mkdir -p /server/log/redis
 mkdir -p /server/www
+touch /server/log/mongodb6/mongodb.log
+rm /etc/my.cnf
+cp /server/etc/mysql/my.cnf /etc/
